36a3f9c96ee785c9ce4a0ed9b22d7c42ef629013
Matthew Britton <100739443+RuleBrittonica@users.noreply.github.com>
2024-07-29 15:39:30 +1000

    Push From Laptop

---
+++ rust_experiments/snake-gui-rs/src/main.rs
@@ -0,0 +1,14 @@
+mod game;
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
