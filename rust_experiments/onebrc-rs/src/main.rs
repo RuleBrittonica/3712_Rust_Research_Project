32f792bd8cc1bda01a1a87c7d8c9da5a145a1c52
RuleBrittonica <mbritton460@gmail.com>
2024-07-31 23:11:38 +1000

    Started working on the 1BRC

--- rust_experiments/onebrc-rs/src/main.rs
+++ rust_experiments/onebrc-rs/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct State {
+    min: f64,
+    max: f64,
+    count: u64,
+    sum: f64
 }
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            min: f64::MAX,
+            max: f64::MIN,
+            count: 0,
+            sum: 0.0,
+        }
+    }
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let avg = self.sum / (self.count as f64);
+        write!(f, "{:.1}/{avg:.1}/{:.1}", self.min, self.max)
+    }
+}
+
+impl State {
+    fn update(&mut self, value: f64) {
+        self.min = self.min.min(value);
+        self.max = self.max.max(value);
+        self.count += 1;
+        self.sum += value;
+    }
+
+    fn merge(&mut self, other: &Self){
+        self.min = self.min.min(other.min);
+        self.max = self.max.max(other.max);
+        self.count += other.count;
+        self.sum += other.sum;
+    }
+}
+
+fn main() {
+
+}
+
+
